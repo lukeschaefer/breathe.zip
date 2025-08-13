276bd2a63c0b3e7c77904ff1b4548f215bb2a332
LukeSchaefer <yathern@gmail.com>
2025-08-13 13:45:07 -0400

    single file

--- breathe.zip/vite.config.ts
+++ breathe.zip/vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
 	plugins: [
+		viteSingleFile(),
 		VitePWA({
 			registerType: 'autoUpdate',
 			injectRegister: 'auto',
